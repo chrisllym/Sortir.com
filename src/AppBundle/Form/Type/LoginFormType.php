d6270e22cea45295273027079b35c78a6ed366c7
chris <laviperefolle@gmail.com>
2019-03-18 16:36:12 +0100

    Configuration du config.yml pour utiliser les formulaires de bootstrap et class LoginFormType dans le dossier Form/Type

--- src/AppBundle/Form/Type/LoginFormType.php
+++ src/AppBundle/Form/Type/LoginFormType.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vin-d
- * Date: 18/03/2019
- * Time: 16:01
- */
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginFormType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder        ->add('identifiant', TextType::class)
+                        ->add('password', PasswordType::class)
+
+
+                        ->add('connexion', SubmitType::class)
+                        ->add('remember', CheckboxType::class)
+                        ->add('forget', UrlType::class);
+    }
+}
