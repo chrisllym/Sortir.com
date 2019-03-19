947aeafc6918de26f72ea19f3c6a0f8a632edc17
fhoudayer <fl.houdayer@gmail.com>
2019-03-19 11:50:39 +0100

    update navbar et ajout favicon lul

---
+++ src/AppBundle/Controller/ProfilController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DWWM
+ * Date: 19/03/2019
+ * Time: 11:07
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Form\Type\ProfilFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends Controller
+{
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function indexAction(Request $request)
+    {
+        $form = $this->createForm(ProfilFormType::class);
+
+        $form->handleRequest($request);
+
+        return $this->render('profil/profil.html.twig' ,
+            ['form' => $form->createView()]);
+    }
+}
