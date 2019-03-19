f77394a4f57fefc13a79430717b4ddd49a3c6f7f
chris <laviperefolle@gmail.com>
2019-03-19 13:55:25 +0100

    Modifications de DefaultController

---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vin-d
+ * Date: 19/03/2019
+ * Time: 12:16
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Participants;
+use AppBundle\Form\Type\LoginFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/login",
+     *     name="login",
+     *     methods={"POST", "GET"}
+     *     )
+     */
+    public function loginAction(Request $request) {
+        $participant = new Participants();
+        $form = $this -> createForm(LoginFormType::class, $participant);
+        $form -> handleRequest($request);
+
+        if($form -> isSubmitted() && $form -> isValid() ) {
+            dump($form -> getData() );
+        }
+        return $this -> render('@App/login.html.twig',
+            [
+                "form"      => $form        -> createView(),
+                "pseudo"    => $participant -> getPseudo(),
+                "motDePasse"    => $participant -> getMotDePasse(),
+            ]);
+    }
+}
--- src/AppBundle/Form/Type/LoginFormType.php
+++ src/AppBundle/Form/Type/LoginFormType.php
@@ -2,23 +2,14 @@
 namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class LoginFormType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder        ->add('identifiant', TextType::class)
-                        ->add('password', PasswordType::class)
-
-
-                        ->add('connexion', SubmitType::class)
-                        ->add('remember', CheckboxType::class)
-                        ->add('forget', UrlType::class);
+        $builder        ->add('pseudo', TextType::class)
+                        ->add('motDePasse', PasswordType::class);
     }
 }
