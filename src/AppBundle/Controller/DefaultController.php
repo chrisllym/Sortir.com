e053b5a351eccf936442e189898132a1bcacba19
chris <laviperefolle@gmail.com>
2019-03-19 12:08:04 +0100

    Modifications de DefaultController et LoginFormType

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Form\Type\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +15,15 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $user = new User();
+        $form = $this -> createForm(LoginFormType::class);
+        $form -> handleRequest($request);
+
+        return $this -> render('@App/default/index.html.twig',
+                                [
+                                    "form"          => $form -> createView(),
+                                    "identifiant"   => $user -> getIdentifiant(),
+                                    'password'      => $user -> getPassword(),
+                                ]);
     }
 }
